186fd2a9c448968ddd29d98d6ee36c8034aa4658
erikmjohnson <erikarisma98@gmail.com>
2019-07-17 12:22:04 -0700

    worked out the state

--- main/src/component/JobsSaved/JobsSaved.js
+++ main/src/component/JobsSaved/JobsSaved.js
@@ -5,7 +5,9 @@ import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 export class JobsSaved extends Component {
-
+  consoleState = () =>{
+    console.log(this.props.profileState);
+  };
 
   render() {
     return (
@@ -13,6 +15,7 @@ export class JobsSaved extends Component {
         <Typography>
         <li>
           <Link component={RouterLink} to="/user"> Search Page </Link>
+          <button onClick={this.consoleState}>State Profile</button>
         </li>
         </Typography>
 
