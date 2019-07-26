16c102223b31cf4ddcb8a622cd8944f9ec09a193
erikmjohnson <erikarisma98@gmail.com>
2019-07-25 17:33:39 -0700

    some css

--- main/src/component/NavBar/NavBar.js
+++ main/src/component/NavBar/NavBar.js
@@ -2,17 +2,13 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import './_banner.scss';
 
 const NavBar = () => {
   return(
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h4" color="inherit">
-            JOBBERWOCKY - A Job Search App
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <div id='banner'>
+      <h1>JOBBERWOCKY</h1>
+      <h2>Find the job you're looking for</h2>
     </div>
   )
 };
